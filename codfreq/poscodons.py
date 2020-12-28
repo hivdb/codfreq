29c69bf105dd9637d41c391e49b85a52477c8c9d
Philip Tzou <philip.npc@gmail.com>
2020-12-28 15:57:55 -0800

    Fix a bug that in-frame deletions are sometime not included; also improve to handle out-frame deletions

--- codfreq/poscodons.py
+++ codfreq/poscodons.py
@@ -4,7 +4,6 @@ import os
 # import ray
 from tqdm import tqdm
 from statistics import mean
-from more_itertools import chunked
 from collections import defaultdict, Counter
 
 from .json_progress import JsonProgress
@@ -18,7 +17,26 @@ ERR_TOO_SHORT = 0b01
 ERR_LOW_QUAL = 0b10
 
 
-def extract_codons(seq, qua, aligned_pairs, ref_offset, site_quality_cutoff):
+def extract_codons(
+    read,
+    ref_offset, site_quality_cutoff,
+    keep_outframe=True
+):
+    """
+    Extract codons from samtools read object
+
+    :param read: the samtools read object
+    :param ref_offset: reference position offset specified by
+        --reference-start
+    :param site_quality_cutoff: site Phred score cutoff specified by
+        --site-quality-cutoff
+    :param keep_outframe: boolean option for keeping/dropping out-frame
+        deletions; out-frame insertions are always preseved
+    """
+
+    seq = read.query_sequence
+    qua = read.query_qualities
+    aligned_pairs = read.get_aligned_pairs(False)
 
     # pre-filter
     err = ERR_OK
@@ -33,45 +51,38 @@ def extract_codons(seq, qua, aligned_pairs, ref_offset, site_quality_cutoff):
 
     codons = {}
     codonqs = defaultdict(list)
-    prev_refpos = 0
-    prev_seqpos = 0
+    refpos_start = 0
+    seqpos_start = 0
     min_cdpos = 0xffffffff
     max_cdpos = 0
-    for seqpos, refpos in aligned_pairs:
-        if refpos:
-            refpos -= ref_offset
-        if refpos is None:
+    for seqpos_end, refpos_end in aligned_pairs:
+        if refpos_end:
+            refpos_end -= ref_offset
+        if refpos_end is None:
             # insertion
-            refpos = prev_refpos - 1
-        if seqpos is None:
+            refpos_end = refpos_start - 1
+        if seqpos_end is None:
             # deletion
-            seqpos = prev_seqpos - 1
+            seqpos_end = seqpos_start - 1
 
-        if refpos > -1:
-            codonpos = refpos // 3 + 1
-            codonbp = refpos % 3
+        if refpos_end > -1:
+            codonpos = refpos_end // 3 + 1
+            codonbp = refpos_end % 3
             min_cdpos = min(min_cdpos, codonpos)
             nas = []
-            for n, q in zip(seq[prev_seqpos:seqpos + 1],
-                            qua[prev_seqpos:seqpos + 1]):
+            for n, q in zip(seq[seqpos_start:seqpos_end + 1],
+                            qua[seqpos_start:seqpos_end + 1]):
                 if q < site_quality_cutoff:
                     n = '*'
                 nas.append(n)
                 codonqs[codonpos].append(q)
             if codonpos not in codons:
                 codons[codonpos] = ['-'] * 3
-            codons[codonpos][codonbp] = ''.join(nas)
+            if nas:
+                codons[codonpos][codonbp] = ''.join(nas)
             max_cdpos = max(max_cdpos, codonpos)
-        prev_seqpos = seqpos + 1
-        prev_refpos = refpos + 1
-    # if prev_seqpos > -1:
-    #     codonpos = refpos // 3 + 1
-    #     codonbp = refpos % 3
-    #     if codonpos not in codons:
-    #         codons[codonpos] = ['-'] * 3
-    #     codons[codonpos][codonbp] = seq[prev_seqpos]
-    #     codonqs[codonpos].append(qua[prev_seqpos])
-    #     max_cdpos = codonpos
+        seqpos_start = seqpos_end + 1
+        refpos_start = refpos_end + 1
     results = []
     for cdpos in range(min_cdpos, max_cdpos + 1):
         codon = ''.join(codons.get(cdpos, ['---']))
@@ -80,9 +91,15 @@ def extract_codons(seq, qua, aligned_pairs, ref_offset, site_quality_cutoff):
             meanq = sum(qs) / len(qs)
         else:
             meanq = 0
-        codon = codon if codon == '---' else codon.replace('-', '')
-        # codon = codon.replace('-', '')
-        if len(codon) - codon.count('*') < 3:
+        codon_wofs = codon if codon == '---' else codon.replace('-', '')
+        if len(codon_wofs) < 3 and not keep_outframe:
+            # drop out-frame deletions
+            continue
+        if cdpos == min_cdpos and codon[:1] in ('-', '*'):
+            # the left-most codon is a partial codon, e.g. --A
+            continue
+        elif cdpos == max_cdpos and codon[-1:] in ('-', '*'):
+            # the right-most codon is a partial codon, e.g. A--
             continue
         codon = codon.replace('*', 'N')
         results.append((cdpos, codon, meanq))
@@ -123,26 +140,15 @@ def iter_poscodons(all_paired_reads,
     else:
         pbar = tqdm(total=total)
         pbar.set_description('Processing {}'.format(description))
-    # max_concurrency = 5
-    for partial in chunked(all_paired_reads, 1000):
-        headers = []
-        input_data = []
-        for header, pair in partial:
-            pbar.update(1)
-            for read in pair:
-                headers.append(header)
-                input_data.append((
-                    read.query_sequence,
-                    read.query_qualities,
-                    read.get_aligned_pairs(False)
-                ))
-        all_results = batch_extract_codons(
-            input_data,
-            ref_offset=reference_start - 1,
-            site_quality_cutoff=site_quality_cutoff,
-        )
-        for header, (results, err, len_seq, mean_qua) in \
-                zip(headers, all_results):
+
+    for header, pair in all_paired_reads:
+        pbar.update(1)
+        for read in pair:
+            results, err, len_seq, mean_qua = extract_codons(
+                read,
+                ref_offset=reference_start - 1,
+                site_quality_cutoff=site_quality_cutoff
+            )
             poscodons = defaultdict(Counter)
             if err & ERR_TOO_SHORT or err & ERR_LOW_QUAL:
                 continue
