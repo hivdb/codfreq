94915e2a64dad914027fcba9e77a367bf7a53fef
Philip Tzou <philip.npc@gmail.com>
2021-04-28 21:40:46 -0700

    Implement codonalign_consensus.py

--- codfreq/codonalign_consensus.py
+++ codfreq/codonalign_consensus.py
@@ -1,107 +1,109 @@
 # cython: profile=True
 
-from collections import Counter
+from postalign.utils.group_by_codons import group_by_codons
+from postalign.processors.codon_alignment import codon_align
 from postalign.models.sequence import Sequence, PositionalSeqStr
 from itertools import groupby
+from operator import itemgetter
 
-from .paired_reads import iter_paired_reads
-from .posnas import iter_posnas
 
 ENCODING = 'UTF-8'
 GAP = ord(b'-')
+DEL_CODON = bytearray(b'---')
+CODON_ALIGN_WINDOW_SIZE = 5
+
+
+def get_sequence_obj(seq_bytearray, seqid):
+    return Sequence(
+        header='seq{}'.format(seqid),
+        description='',
+        seqtext=PositionalSeqStr.init_from_nastring(
+            bytes(seq_bytearray).decode(ENCODING)
+        ),
+        seqid=seqid,
+        seqtype='NA',
+        abs_seqstart=0,
+        skip_invalid=True
+    )
+
+
+def assemble_alignment(geneposcdf_lookup, refseq, genedef):
+    gene = genedef['geneName']
+    gene_refstart = genedef['refStart']
+    gene_refend = genedef['refEnd']
+    gene_refseq = bytearray()
+    gene_queryseq = bytearray()
+    refsize = gene_refend - gene_refstart + 1
+    first_aa = refsize // 3
+    last_aa = 0
+
+    for aapos in range(1, refsize // 3 + 1):
+        napos = gene_refstart + aapos * 3 - 4
+        ref_codon = refseq[napos:napos + 3]
+        geneposcdf = geneposcdf_lookup.get((gene, aapos))
+        if geneposcdf:
+            cons_codon = bytearray(geneposcdf[0]['codon'], ENCODING)
+            first_aa = min(first_aa, aapos)
+            last_aa = max(last_aa, aapos)
+        else:
+            cons_codon = DEL_CODON
+        cons_codon_size = len(cons_codon)
+        if cons_codon_size < 3:
+            cons_codon += bytearray([GAP]) * (3 - cons_codon_size)
+        elif cons_codon_size > 3:
+            ref_codon += bytearray([GAP]) * (cons_codon_size - 3)
+        gene_refseq += ref_codon
+        gene_queryseq += cons_codon
+
+    if last_aa == 0:
+        return None, None, None, None
+
+    return (get_sequence_obj(gene_refseq, 1),
+            get_sequence_obj(gene_queryseq, 2),
+            first_aa,
+            last_aa)
 
 
 def codonalign_consensus(codonfreq, ref, genes):
-    return codonfreq
-
-
-def get_consensus_na(posnas):
-    posnas = tuple(posnas)
-    ((_, idx), cons_na), count = posnas[0]
-    if idx == 0:
-        return cons_na
-    else:
-        total = sum(count for _, count in posnas)
-        if count > total * 0.9:
-            return cons_na
-        return None
-
-
-def get_most_common_nas(sampath):
-    nafreqs = Counter()
-    all_paired_reads = iter_paired_reads(sampath)
-
-    # flatten posnas generator (remove headers)
-    nafreqs.update(
-        ((pos, idx), na)
-        for _, posnas in iter_posnas(all_paired_reads)
-        for (pos, idx), na, _ in posnas
-    )
-    return {
-        posidx: get_consensus_na(items)
-        for posidx, items in groupby(
-            sorted(
-                nafreqs.items(),
-                key=lambda item: (
-                    item[0][0],  # posidx
-                    -item[1]     # -count
-                )
-            ),
-            lambda item: item[0][0]  # posidx
-        )
-        if posidx[1] == 0  # exclude all insertions when building consensus
+    refseq = bytearray(ref['refSequence'], ENCODING)
+    geneposcdf_lookup = {
+        genepos: sorted(genecdf, key=lambda cdf: -cdf['count'])
+        for genepos, genecdf in
+        groupby(codonfreq, key=itemgetter('gene', 'position'))
     }
+    for genedef in genes:
+        gene = genedef['geneName']
 
-
-def iter_alignment_pairs(refseq_bytes, most_common_nas):
-    for refpos0, refna in enumerate(refseq_bytes):
-        refpos = refpos0 + 1
-        if (refpos, 0) in most_common_nas:
-            na = most_common_nas[(refpos, 0)]
-            yield refna, na
-            # i = 1
-            # while (refpos, i) in most_common_nas:
-            #     na = most_common_nas[(refpos, i)]
-            #     if not na:
-            #         break
-            #     i += 1
-            #     yield GAP, na
-        else:
-            yield refna, GAP
-
-
-def assemble_alignment(refseq, most_common_nas):
-    refseq_bytes = bytearray(refseq, ENCODING)
-    return zip(*iter_alignment_pairs(refseq_bytes, most_common_nas))
-
-
-def calc_pairwise_consensus(sampath, refseq):
-    most_common_nas = get_most_common_nas(sampath)
-
-    (alignedref,
-     alignedseq) = assemble_alignment(refseq, most_common_nas)
-
-    return (
-        Sequence(
-            header='ref',
-            description='',
-            seqtext=PositionalSeqStr.init_from_nastring(
-                bytes(alignedref).decode(ENCODING)
-            ),
-            seqid=1,
-            seqtype='NA',
-            abs_seqstart=0,
-            skip_invalid=True
-        ),
-        Sequence(
-            header=sampath,
-            description='',
-            seqtext=PositionalSeqStr.init_from_nastring(
-                bytes(alignedseq).decode(ENCODING)
-            ),
-            seqid=2,
-            seqtype='NA',
-            abs_seqstart=0,
-            skip_invalid=True
+        (gene_refseq_obj,
+         gene_queryseq_obj,
+         first_aa,
+         last_aa) = assemble_alignment(
+             geneposcdf_lookup, refseq, genedef
         )
-    )
+
+        if last_aa is None:
+            continue
+
+        (gene_refseq_obj,
+         gene_queryseq_obj) = codon_align(
+             gene_refseq_obj, gene_queryseq_obj,
+             window_size=CODON_ALIGN_WINDOW_SIZE,
+             refstart=first_aa * 3 - 2,
+             refend=last_aa * 3
+        )
+        for aapos0, (refcodon, querycodon) in enumerate(zip(
+            *group_by_codons(
+                gene_refseq_obj.seqtext,
+                gene_queryseq_obj.seqtext
+            )
+        )):
+            aapos = aapos0 + 1
+            geneposcdf = geneposcdf_lookup.get((gene, aapos))
+            if not geneposcdf:
+                continue
+            if gene == 'S' and aapos in (68, 69, 70):
+                print(gene, aapos, querycodon)
+            geneposcdf[0]['codon'] = str(
+                PositionalSeqStr.join(querycodon)
+            )
+            yield from geneposcdf
